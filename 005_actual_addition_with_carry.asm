7c7766fee7e40ecb59775f17116200efc820cb4c
siyam-hosssain <siyam.cybersoul@gmail.com>
2025-11-12 12:25:02 +0600

    actual addition and carry

---
+++ 005_actual_addition_with_carry.asm
@@ -0,0 +1,30 @@
+.model small
+.stack 100h
+.data
+
+a dw 1234h
+b dw 6789h
+sum dw ?
+carry db 00h
+
+.code
+main proc
+    
+    mov ax, @data
+    mov ds, ax
+    
+    mov ax, a
+    mov bx, b
+    
+    add ax, bx  
+    
+    
+    jnc nocarry
+        inc carry
+    nocarry:
+    
+    mov sum, ax  
+        
+        
+    main endp
+end main
